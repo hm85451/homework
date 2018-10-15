83b164e225d39fb9a1e2ab5512a7c96e7339538b
hm85451 <43687967+hm85451@users.noreply.github.com>
2018-10-14 22:15:28 -0700

    Update pi.cpp

--- pi.cpp
+++ pi.cpp
@@ -5,6 +5,11 @@ using namespace std;
 int main(){
 	double len=0;
 	double sum=0;
+	
+	cout.setf(ios::fixed);
+        cout.setf(ios::showpoint);
+        cout.precision(3);
+
 	while(true){
 	sum=0;
 	cout<<"Enter the value of the parameter 'n' in the Leibniz formula (or -1 to quit):"<<endl;
@@ -12,10 +17,11 @@ int main(){
 	if(len<0)
 		break;
 	
-	for (int i = 0; i<len; i++){
-	sum+=4.0*(pow(-1.0, i))/(2*(double)i + 1);
+	for (int i = 0; i<=len; i++){
+		sum+=pow(-1.0, i)/(2*(double)i + 1);
 	}
-	cout<<"The approximate value of pi using 1 term is: " <<sum<<endl;
+	sm*=4;
+	cout<<"The approximate value of pi using "<< len <<" term is: " <<sum<<endl;
 	}
 	return 0;
 }
